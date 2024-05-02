583f626c0f46e2c29a87cf51b12ae7a6e1b28347
AlberTorres <albertotos64@gmail.com>
2024-05-02 14:57:04 -0500

    proyecto inicializado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>esta es la web de funcional</h1>
+      <p>esta hecha por Omar, Alberto, Juan y Ramiro</p>
+    </div>
+  )
+}
+
+export default App
+
